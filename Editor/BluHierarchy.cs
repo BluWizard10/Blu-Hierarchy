42ed4811442511c2637ddeb88d4336553c0fe448
BluWizard10 <17975114+BluWizard10@users.noreply.github.com>
2024-01-11 21:39:14 -0800

    Added Custom Icons for various Component Classes in the Hierarchy.

--- Editor/BluHierarchy.cs
+++ Editor/BluHierarchy.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 using UnityEditor;
 using UnityEngine.EventSystems;
 using System.Reflection;
+#if VRC_SDK_VRCSDK3
+using VRC.SDK3.Dynamics.PhysBone.Components;
+#endif
 
 namespace BluWizard.Hierarchy
 {
     [InitializeOnLoad]
     public static class BluHierarchy
     {
+
         static BluHierarchy()
         {
             // Subscribe to the Editor's hierarchy window item callback
@@ -49,7 +53,7 @@ namespace BluWizard.Hierarchy
                 if (newShowTransformIcon != currentShowTransformIcon)
                 {
                     BluHierarchySettings.ShowTransformIcon = newShowTransformIcon;
-                    BluHierarchy.RepaintHierarchyWindow();
+                    RepaintHierarchyWindow();
                 }
             }
         }
@@ -109,87 +113,134 @@ namespace BluWizard.Hierarchy
 
                 if (component is Transform && !BluHierarchySettings.ShowTransformIcon) continue;
 
-                Texture2D icon = GetCustomIcon(component.GetType());
+                Texture2D icon = null;
+
+                // Load Custom Icons for VRC Avatar SDK Components
+                if (component.GetType().Name == "VRCAvatarDescriptor") { icon = Resources.Load<Texture2D>("Icons/vrcAvatarDescriptor"); }
+                else if (component.GetType().Name == "PipelineManager") { icon = Resources.Load<Texture2D>("Icons/vrcPipelineManager"); }
+                else if (component.GetType().Name == "VRCPhysBone") { icon = Resources.Load<Texture2D>("Icons/vrcPhysBone"); }
+                #if VRC_SDK_VRCSDK3 // Prevent borking Script if VRC SDK does not exist in the project.
+                else if (component.GetType().Name == "VRCPhysBoneCollider")
+                {
+                    var physBoneCollider = component as VRCPhysBoneCollider;
+                    switch (physBoneCollider.shapeType)
+                    {
+                        case VRC.Dynamics.VRCPhysBoneColliderBase.ShapeType.Plane:
+                            icon = Resources.Load<Texture2D>("Icons/vrcPhysBoneColliderPlane");
+                            break;
+                        case VRC.Dynamics.VRCPhysBoneColliderBase.ShapeType.Sphere:
+                            icon = Resources.Load<Texture2D>("Icons/vrcPhysBoneCollider");
+                            break;
+                        case VRC.Dynamics.VRCPhysBoneColliderBase.ShapeType.Capsule:
+                            icon = Resources.Load<Texture2D>("Icons/vrcPhysBoneCollider");
+                            break;
+                    }
+                }
+                #endif
+                else if (component.GetType().Name == "VRCContactReceiver") { icon = Resources.Load<Texture2D>("Icons/vrcContactReceiver"); }
+                else if (component.GetType().Name == "VRCContactSender") { icon = Resources.Load<Texture2D>("Icons/vrcContactSender"); }
+                else if (component.GetType().Name == "VRCImposterSettings") { icon = Resources.Load<Texture2D>("Icons/vrcImposterSettings"); }
+
+                // Load Custom Icons for VRC World SDK Components
+                else if (component.GetType().Name == "VRCSceneDescriptor") { icon = Resources.Load<Texture2D>("Icons/vrcSceneDescriptor"); }
+                else if (component.GetType().Name == "UdonBehaviour") { icon = Resources.Load<Texture2D>("Icons/vrcUdonBehaviour"); }
+                else if (component.GetType().Name == "VRCPickup") { icon = Resources.Load<Texture2D>("Icons/vrcPickup"); }
+                else if (component.GetType().Name == "VRCMirrorReflection") { icon = Resources.Load<Texture2D>("Icons/vrcMirrorReflection"); }
+                else if (component.GetType().Name == "VRCStation") { icon = Resources.Load<Texture2D>("Icons/vrcStation"); }
+                // else if (component.GetType().Name == "VRCObjectSync") { icon = Resources.Load<Texture2D>("Icons/vrcObjectSync"); }
+                // else if (component.GetType().Name == "VRCObjectPool") { icon = Resources.Load<Texture2D>("Icons/vrcObjectPool"); }
+                // else if (component.GetType().Name == "VRCPortalMarker") { icon = Resources.Load<Texture2D>("Icons/vrcPortalMarker"); }
+                // else if (component.GetType().Name == "VRCAvatarPedestal") { icon = Resources.Load<Texture2D>("Icons/vrcAvatarPedestal"); }
+
+                // Load Custom Icons for Third-Party Utility Components
+                else if (component.GetType().Name == "VRCFury") { icon = Resources.Load<Texture2D>("Icons/VRCFury"); }
+
+                // if no match, use Unity's Default Icons
+                else
+                {
+                    icon = GetCustomIcon(component.GetType());
+                }
 
                 if (icon == null)
                 {
                     GUIContent iconContent = EditorGUIUtility.ObjectContent(component, component.GetType());
                     icon = iconContent.image as Texture2D;
+                }
 
-                    Rect iconRect = new Rect(currentX, selectionRect.y, iconSize, iconSize);
+                Rect iconRect = new Rect(currentX, selectionRect.y, iconSize, iconSize);
 
-                    bool isEnabled = false;
-                    bool canToggle = false;
+                bool isEnabled = false;
+                bool canToggle = false;
 
-                    // Check for components that can be toggled and set the isEnabled flag
-                    if (component is Behaviour behaviourComponent)
+                // Check for components that can be toggled and set the isEnabled flag
+                if (component is Behaviour behaviourComponent)
+                {
+                    isEnabled = behaviourComponent.enabled;
+                    canToggle = true;
+                }
+                else if (component is Renderer rendererComponent)
+                {
+                    isEnabled = rendererComponent.enabled;
+                    canToggle = true;
+                }
+
+                // Component Toggle functionality
+                if (!canToggle && component is Component)
+                {
+                    PropertyInfo enabledProperty = component.GetType().GetProperty("enabled", BindingFlags.Instance | BindingFlags.Public);
+                    if (enabledProperty != null && enabledProperty.PropertyType == typeof(bool))
                     {
-                        isEnabled = behaviourComponent.enabled;
+                        isEnabled = (bool)enabledProperty.GetValue(component, null);
                         canToggle = true;
                     }
-                    else if (component is Renderer rendererComponent)
-                    {
-                        isEnabled = rendererComponent.enabled;
-                        canToggle = true;
-                    }
+                }
 
-                    // Component Toggle functionality
-                    if (!canToggle && component is Component)
-                    {
-                        PropertyInfo enabledProperty = component.GetType().GetProperty("enabled", BindingFlags.Instance | BindingFlags.Public);
-                        if (enabledProperty != null && enabledProperty.PropertyType == typeof(bool))
-                        {
-                            isEnabled = (bool)enabledProperty.GetValue(component, null);
-                            canToggle = true;
-                        }
-                    }
+                // Collider Component Toggle functionality
+                if (component is Collider colliderComponent)
+                {
+                    isEnabled = colliderComponent.enabled;
+                    canToggle = true;
 
-                    // Collider Component Toggle functionality
-                    if (component is Collider colliderComponent)
-                    {
-                        isEnabled = colliderComponent.enabled;
-                        canToggle = true;
-
-                        if (canToggle && GUI.Button(iconRect, GUIContent.none, GUIStyle.none))
-                        {
-                            Undo.RecordObject(colliderComponent, "Toggle Collider");
-                            colliderComponent.enabled = !isEnabled;
-                            EditorUtility.SetDirty(colliderComponent);
-                        }
-                    }
-
-                    // Set the color for the icon based on the enabled state and if it's toggleable
-                    Color prevColor = GUI.color;
-                    GUI.color = canToggle ? (isEnabled ? Color.white : new Color(1f, 1f, 1f, 0.5f)) : Color.white;
-
-                    // Draw the texture with the modified color
-                    GUI.DrawTexture(iconRect, icon);
-
-                    // Reset color to previous state
-                    GUI.color = prevColor;
-
-                    // Only add button functionality if the component can be toggled
                     if (canToggle && GUI.Button(iconRect, GUIContent.none, GUIStyle.none))
                     {
-                        Undo.RecordObject(component, "Toggle Component");
-                        isEnabled = !isEnabled;
+                        Undo.RecordObject(colliderComponent, "Toggle Collider");
+                        colliderComponent.enabled = !isEnabled;
+                        EditorUtility.SetDirty(colliderComponent);
+                    }
+                }
 
-                        // Apply the toggled state back to the component
-                        if (component is Behaviour)
-                        {
-                            (component as Behaviour).enabled = isEnabled;
-                        }
-                        else if (component is Renderer)
-                        {
-                            (component as Renderer).enabled = isEnabled;
-                        }
+                // Set the color for the icon based on the enabled state and if it's toggleable
+                Color prevColor = GUI.color;
+                GUI.color = canToggle ? (isEnabled ? Color.white : new Color(1f, 1f, 1f, 0.5f)) : Color.white;
 
-                        EditorUtility.SetDirty(component);
+                // Draw the texture with the modified color
+                GUI.DrawTexture(iconRect, icon);
+
+                // Reset color to previous state
+                GUI.color = prevColor;
+
+                // Only add button functionality if the component can be toggled
+                if (canToggle && GUI.Button(iconRect, GUIContent.none, GUIStyle.none))
+                {
+                    Undo.RecordObject(component, "Toggle Component");
+                    isEnabled = !isEnabled;
+
+                    // Apply the toggled state back to the component
+                    if (component is Behaviour)
+                    {
+                        (component as Behaviour).enabled = isEnabled;
+                    }
+                    else if (component is Renderer)
+                    {
+                        (component as Renderer).enabled = isEnabled;
                     }
 
-                    // Adjust currentX for the next icon
-                    currentX -= iconSize + 2; // icon size plus some padding
+                    EditorUtility.SetDirty(component);
                 }
+
+                // Adjust currentX for the next icon
+                currentX -= iconSize + 2; // icon size plus some padding
+
             }
             // Draw EditorOnly Text
             DrawEditorOnlyText(go, selectionRect);
